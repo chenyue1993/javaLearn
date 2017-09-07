5be60dbf4bdf9000fca07d0c045a96a946f61bab
chenyue <qq1448771000@163.com>
2017-09-07 16:53:22 +0800

    add mybatis mapper

---
+++ java/SQL/createUsername.sql
@@ -0,0 +1,18 @@
+﻿drop table if exists emp;
+create table emp(
+  empno int(4) primary key auto_increment,
+  ename varchar(10) not null,
+  hiredate varchar(10),
+  sal varchar(10) 
+);
+
+insert into emp(ename,hiredate,sal) values('zhangsan','2017-7-12','6000')；
+
+drop table if exists t_user;
+create table t_user(
+  username varchar(10) primary key ,
+  password varchar(10) not null
+
+);
+
+insert into t_user values('zhangsan','123');
